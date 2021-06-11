d138904ebd1c06d4b98bee0db2a8921e585fe9cd
Leonardo <oliveirabalsa2@gmail.com>
2021-06-10 22:58:57 -0300

    chore: update project structure

---
+++ src/test/kotlin/br/com/microservice/kotlinkafka/KotlinKafkaApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.microservice.kotlinkafka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinKafkaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
